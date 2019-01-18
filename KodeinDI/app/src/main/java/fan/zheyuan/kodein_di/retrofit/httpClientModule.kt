7834738ecb8ceecdebb122b0d23f705cb95084f3
Fan Zheyuan <fanzheyuan@le.com>
2019-01-18 13:18:34 +0800

    add dependencies.gradle and use it.

---
+++ KodeinDI/app/src/main/java/fan/zheyuan/kodein_di/retrofit/httpClientModule.kt
@@ -0,0 +1,11 @@
+package fan.zheyuan.kodein_di.retrofit
+
+import org.kodein.di.Kodein
+
+const val HTTP_CLIENT_MODULE_TAG = "httpClientModule"
+
+const val TIME_OUT_SECONDS = 20
+
+val httpClientModule = Kodein.Module(HTTP_CLIENT_MODULE_TAG) {
+//    bind<R
+}
